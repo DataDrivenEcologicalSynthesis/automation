3ed6ad752874c75e6545bb28e10123b01ecb7eb7
Timothée Poisot <timothee.poisot@umontreal.ca>
2020-05-21 20:29:55 -0400

    Update get_occurrences.jl

--- scripts/get_occurrences.jl
+++ scripts/get_occurrences.jl
@@ -9,11 +9,9 @@ sp = GBIF.taxon(sp_name)
 
 
 # Get 900 occurrences
-occ = GBIF.occurrences(sp, "has_coordinates" => true, "limit" => 300)
+occ = GBIF.occurrences(sp, "hasCoordinate" => true, "limit" => 300)
 GBIF.occurrences!(occ)
 GBIF.occurrences!(occ)
 
-ispath("data") && mkdir("data")
-
-CSV.write(DataFrame(occ), "data/occurrences.csv")
+CSV.write(DataFrame(occ), "occurrences.csv")
 
