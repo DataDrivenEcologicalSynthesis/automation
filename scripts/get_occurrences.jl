416c3e0cd33f239244b32e65b5949b114930d04c
Timothée Poisot <timothee.poisot@umontreal.ca>
2020-05-21 21:45:39 -0400

    Update get_occurrences.jl

--- scripts/get_occurrences.jl
+++ scripts/get_occurrences.jl
@@ -4,16 +4,22 @@ using DataFrames
 import CSV
 
 
-sp_name = "Amphiprion ocellaris"
-sp = GBIF.taxon(sp_name)
+sp_name = "Amphiprion"
+sp = GBIF.taxon(sp_name, strict=false)
 
 
 # Get 900 occurrences
 occ = GBIF.occurrences(sp, "hasCoordinate" => "true", "limit" => 300)
 GBIF.occurrences!(occ)
 GBIF.occurrences!(occ)
+GBIF.occurrences!(occ)
 
-data = select(DataFrame(occ), [:longitude, :latitude])
+raw_data = DataFrame(occ)
+raw_data = raw_data[raw_data.rank .== "SPECIES", :]
+
+data = select(raw_data, [:name, :longitude, :latitude])
+
+first(data, 5)
 
 CSV.write("occurrences.csv", data; writeheader=false)
 
