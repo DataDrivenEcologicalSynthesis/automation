9c2c1d48141f234d15e50d9fb5573eb0bd545b78
Timothée Poisot <timothee.poisot@umontreal.ca>
2020-05-21 20:43:31 -0400

    Create make_plot.py

---
+++ scripts/make_plot.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x, y = np.loadtxt('example.txt', delimiter=',', unpack=True)
+plt.plot(x,y, label='Loaded from file!')
+
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Interesting Graph\nCheck it out')
+plt.legend()
+plt.show()
